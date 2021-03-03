rotation to gamma angle fixed

analyzer_test2.py
@@ -24,24 +24,32 @@ for tr in traj_list_copy:
         continue
 
     tr = hb.pass_to_slits(tr, dt, E, B, geomT15,
-                          target='slit', timestep_divider=15,
+                          target='slit', timestep_divider=10,
                           no_intersect=True, no_out_of_bounds=True)
-    # hbplot.plot_traj_toslits(tr, geomT15, Btor, Ipl, plot_fan=True)
+    hbplot.plot_traj_toslits(tr, geomT15, Btor, Ipl, plot_fan=True)
 
 
     # tr = hb.pass_to_slits(tr, dt, E, B, geomT15,
     #                       target='det', timestep_divider=15,
     #                       no_intersect=True, no_out_of_bounds=True)
     # hbplot.plot_traj_toslits(tr, geomT15, Btor, Ipl, plot_fan=True)
+
     for i_slit in range(n_slits):
+        rv_list = []
         for fan_tr in tr.RV_sec_toslits[i_slit]:
             RV0 = np.array([fan_tr[0]])
+            # pass traj to detector
             tr.pass_sec(RV0, geomT15.r_dict['det'], E, B, geomT15, tmax=9e-5,
-                        eps_xy=1e-3, eps_z=1)
+                        eps_xy=1, eps_z=1)
+            if not (tr.IntersectGeometrySec or tr.B_out_of_bounds):
+                rv_list.append(fan_tr)
+        # update RV list
+        tr.RV_sec_toslits[i_slit] = rv_list
+
     break
 
 # %% plot trajectories
 
 # hbplot.plot_traj(traj_list_copy, geomT15, 240., UA2, Btor, Ipl,
 #                   full_primary=False, plot_analyzer=True)
-hbplot.plot_traj_toslits(tr, geomT15, Btor, Ipl, plot_fan=True)
+# hbplot.plot_traj_toslits(tr, geomT15, Btor, Ipl, plot_fan=True)

hibplib.py
@@ -511,6 +511,7 @@ def plot_slits(r_slits, spot, ax, axes='XY', n_slit='all', color='g'):
 # %%
 def define_slits(r0, angles, n_slits, slit_dist, slit_w, slit_l):
     slit_alpha, slit_beta, slit_gamma = angles
+    axis = calc_vector(1, slit_alpha, slit_beta)
     n_slits = int(n_slits)
     # calculate slits coordinates:
     r_slits = np.zeros([n_slits, 5, 3])
@@ -526,11 +527,11 @@ def define_slits(r0, angles, n_slits, slit_dist, slit_w, slit_l):
         # rotate and shift to slit position:
         for j in range(5):
             r_slits[i_slit, j, :] = rotate(r_slits[i_slit, j, :],
-                                           axis=(1, 0, 0), deg=slit_gamma)
-            r_slits[i_slit, j, :] = rotate(r_slits[i_slit, j, :],
                                            axis=(0, 0, 1), deg=slit_alpha)
             r_slits[i_slit, j, :] = rotate(r_slits[i_slit, j, :],
                                            axis=(0, 1, 0), deg=slit_beta)
+            r_slits[i_slit, j, :] = rotate(r_slits[i_slit, j, :],
+                                           axis=axis, deg=slit_gamma)
             r_slits[i_slit, j, :] += r0
 
     # calculate normal to slit plane:
@@ -1137,6 +1138,7 @@ def plate_flags(range_x, range_y, range_z, U,
                 plts_geom, plts_angles, plts_center):
 
     alpha, beta, gamma = plts_angles
+    axis = calc_vector(1, alpha, beta)
     length, width, thick, gap = plts_geom
 
     # Geometry rotated in system based on central point between plates
@@ -1153,11 +1155,11 @@ def plate_flags(range_x, range_y, range_z, U,
     UP_rotated = UP_full.copy()
     for i in range(UP_full.shape[0]):
         UP_rotated[i, :] = rotate(UP_rotated[i, :],
-                                  axis=(1, 0, 0), deg=gamma)
-        UP_rotated[i, :] = rotate(UP_rotated[i, :],
                                   axis=(0, 0, 1), deg=alpha)
         UP_rotated[i, :] = rotate(UP_rotated[i, :],
                                   axis=(0, 1, 0), deg=beta)
+        UP_rotated[i, :] = rotate(UP_rotated[i, :],
+                                  axis=axis, deg=gamma)
         # shift coords center
         UP_rotated[i, :] += plts_center
 
@@ -1174,11 +1176,11 @@ def plate_flags(range_x, range_y, range_z, U,
     LP_rotated = LP_full.copy()
     for i in range(LP_full.shape[0]):
         LP_rotated[i, :] = rotate(LP_rotated[i, :],
-                                  axis=(1, 0, 0), deg=gamma)
-        LP_rotated[i, :] = rotate(LP_rotated[i, :],
                                   axis=(0, 0, 1), deg=alpha)
         LP_rotated[i, :] = rotate(LP_rotated[i, :],
                                   axis=(0, 1, 0), deg=beta)
+        LP_rotated[i, :] = rotate(LP_rotated[i, :],
+                                  axis=axis, deg=gamma)
         # shift coords center
         LP_rotated[i, :] += plts_center
 
@@ -1202,9 +1204,12 @@ def plate_flags(range_x, range_y, range_z, U,
                    (y >= upper_cube[0, 1]) and (y <= upper_cube[1, 1]) and \
                    (z >= upper_cube[0, 2]) and (z <= upper_cube[1, 2]):
                     r = np.array([x, y, z]) - plts_center
-                    r_rot = rotate(rotate(rotate(r, axis=(0, 1, 0), deg=-beta),
-                                          axis=(0, 0, 1), deg=-alpha),
-                                   axis=(1, 0, 0), deg=-gamma)
+                    # r_rot = rotate(rotate(rotate(r, axis=(0, 1, 0), deg=-beta),
+                    #                       axis=(0, 0, 1), deg=-alpha),
+                    #                axis=(1, 0, 0), deg=-gamma)
+                    r_rot = rotate(rotate(rotate(r, axis=axis, deg=-gamma),
+                                          axis=(0, 1, 0), deg=-beta),
+                                   axis=(0, 0, 1), deg=-alpha)
                     # define masks for upper and lower plates
                     upper_plate_flag[i, j, k] = (r_rot[0] >= -length/2.) and \
                         (r_rot[0] <= length/2.) and (r_rot[2] >= -width/2.) and \
@@ -1215,9 +1220,12 @@ def plate_flags(range_x, range_y, range_z, U,
                    (y >= lower_cube[0, 1]) and (y <= lower_cube[1, 1]) and \
                    (z >= lower_cube[0, 2]) and (z <= lower_cube[1, 2]):
                     r = np.array([x, y, z]) - plts_center
-                    r_rot = rotate(rotate(rotate(r, axis=(0, 1, 0), deg=-beta),
-                                          axis=(0, 0, 1), deg=-alpha),
-                                   axis=(1, 0, 0), deg=-gamma)
+                    # r_rot = rotate(rotate(rotate(r, axis=(0, 1, 0), deg=-beta),
+                    #                       axis=(0, 0, 1), deg=-alpha),
+                    #                axis=(1, 0, 0), deg=-gamma)
+                    r_rot = rotate(rotate(rotate(r, axis=axis, deg=-gamma),
+                                          axis=(0, 1, 0), deg=-beta),
+                                   axis=(0, 0, 1), deg=-alpha)
                     # define masks for upper and lower plates
                     lower_plate_flag[i, j, k] = (r_rot[0] >= -length/2.) and \
                         (r_rot[0] <= length/2.) and (r_rot[2] >= -width/2.) and \

hibpplotlib.py
@@ -259,6 +259,28 @@ def plot_contours_zy(X, Y, Z, U, upper_plate_flag, lower_plate_flag,
 
 
 # %%
+def plot_contours_xz(X, Y, Z, U, upper_plate_flag, lower_plate_flag,
+                     n_contours=30, plates_color='k'):
+
+    fig, ax1 = plt.subplots()
+    set_axes_param(ax1, 'X (m)', 'Z (m)')
+
+    y_cut = U.shape[1]//2
+    ax1.contour(X, Z, U[:, y_cut, :].swapaxes(0, 1), n_contours)
+    # add the edge of the domain
+    domain = patches.Rectangle((min(X), min(Z)), max(X)-min(X), max(Z)-min(Z),
+                               linewidth=2, linestyle='--', edgecolor='k',
+                               facecolor='none')
+    ax1.add_patch(domain)
+
+    x, y, z = np.meshgrid(X, Y, Z, indexing='ij')
+    ax1.plot(x[:, y_cut, :][upper_plate_flag[:, y_cut, :]],
+             z[:, y_cut, :][upper_plate_flag[:, y_cut, :]], 'o', color='k')
+    ax1.plot(x[:, y_cut, :][lower_plate_flag[:, y_cut, :]],
+             z[:, y_cut, :][lower_plate_flag[:, y_cut, :]], 'o', color='k')
+
+
+# %%
 def plot_stream_zy(X, Y, Z, Ex, Ey, Ez, upper_plate_flag, lower_plate_flag,
                    dens=1.0, plates_color='k'):
 
@@ -795,22 +817,23 @@ def plot_grid(traj_list, geom, Btor, Ipl,
 # %%
 def plot_traj_toslits(tr, geom, Btor, Ipl, plot_fan=True, plot_flux=True):
 
-    # fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3)
-    fig, (ax1, ax3) = plt.subplots(nrows=1, ncols=2)
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=1, ncols=3)
+    # fig, (ax1, ax3) = plt.subplots(nrows=1, ncols=2)
 
     set_axes_param(ax1, 'X (m)', 'Y (m)')
-    # set_axes_param(ax2, 'X (m)', 'Z (m)')
+    set_axes_param(ax2, 'X (m)', 'Z (m)')
     set_axes_param(ax3, 'Z (m)', 'Y (m)')
     ax1.set_title('E={} keV, UA2={} kV, Btor={} T, Ipl={} MA'
                   .format(tr.Ebeam, tr.U[0], Btor, Ipl))
 
     # plot geometry
     geom.plot_geom(ax1, axes='XY', plot_aim=False)
+    geom.plot_geom(ax2, axes='XZ', plot_aim=False)
     geom.plot_geom(ax3, axes='ZY', plot_aim=False)
 
     # draw slits
     geom.plot_analyzer(ax1, axes='XY')
-    # geom.plot_analyzer(ax2, axes='XZ')
+    geom.plot_analyzer(ax2, axes='XZ')
     geom.plot_analyzer(ax3, axes='ZY')
 
     n_slits = geom.slits_edges.shape[0]
@@ -822,12 +845,14 @@ def plot_traj_toslits(tr, geom, Btor, Ipl, plot_fan=True, plot_flux=True):
 
     # plot primary trajectory
     tr.plot_prim(ax1, axes='XY', color='k', full_primary=True)
+    tr.plot_prim(ax2, axes='XZ', color='k', full_primary=True)
     tr.plot_prim(ax3, axes='ZY', color='k', full_primary=True)
 
     # plot precise fan of secondaries
     if plot_fan:
         for fan_tr in tr.fan_to_slits:
             ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color='tab:gray')
+            ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color='tab:gray')
             ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color='tab:gray')
 
     # plot secondaries
@@ -835,6 +860,7 @@ def plot_traj_toslits(tr, geom, Btor, Ipl, plot_fan=True, plot_flux=True):
         c = next(colors)
         for fan_tr in tr.RV_sec_toslits[i_slit]:
             ax1.plot(fan_tr[:, 0], fan_tr[:, 1], color=c)
+            ax2.plot(fan_tr[:, 0], fan_tr[:, 2], color=c)
             ax3.plot(fan_tr[:, 2], fan_tr[:, 1], color=c)
 
     # plot zones

laplase_analyzer.py
@@ -46,6 +46,10 @@ if __name__ == '__main__':
     plts_name = 'A4'
     save_data = True
 
+    # define voltages [Volts]
+    Uupper_plate = 0.
+    Ulower_plate = 1e3
+
     # define center position
     plts_center = np.array([0., 0., 0.])  # plates center
     # initially plates are parallel to XZ plane
@@ -107,6 +111,9 @@ if __name__ == '__main__':
 
     elif plts_name == 'an':
         # ANALYZER
+        # define voltages [Volts]
+        Uupper_plate = 1e3
+        Ulower_plate = 0.
         beamline = 'sec'
         # slits configuration [m]
         n_slits, slit_dist, slit_w = 7, 0.01, 5e-3
@@ -124,7 +131,7 @@ if __name__ == '__main__':
 
         length = 1.2*XD  # along X [m]
 
-        alpha, beta, gamma = alpha_sec-theta_an, beta_sec, gamma_sec-180
+        alpha, beta, gamma = alpha_sec-theta_an, beta_sec, gamma_sec
 
         # G coeff of the analyzer
         G = (XD*np.tan(theta_an*drad) - YD) / (4 * gap *
@@ -132,13 +139,11 @@ if __name__ == '__main__':
         G = np.round(G, 5)
 
         # center of the coords system should be shifted to the slit center
-        # distance from coords center to slit center
-        dist = np.sqrt((XD/2)**2 + (gap/2 + YD1)**2)
-        # alpha angle of the vector
-        alpha_shift = np.arctan((YD1 + gap/2) / (XD/2)) / drad  # [deg]
-        # resulting shift vector
-        zero_shift = hb.calc_vector(dist, alpha_shift, beta)
-        plts_center += zero_shift
+        plts_center = np.array([XD/2, gap/2 + YD1, 0])
+        axis = hb.calc_vector(1, alpha, beta)
+        plts_center = hb.rotate(plts_center, axis=(0, 0, 1), deg=alpha)
+        plts_center = hb.rotate(plts_center, axis=(0, 1, 0), deg=beta)
+        plts_center = hb.rotate(plts_center, axis=axis, deg=gamma)
 
         an_params = np.array([n_slits, slit_dist, slit_w, G, theta_an,
                               round(XD, 4), round(YD1, 4), round(YD2, 4)])
@@ -172,10 +177,6 @@ if __name__ == '__main__':
     edge_flag[edge_list, :, :] = True
     edge_flag[:, edge_list, :] = True
     edge_flag[:, :, edge_list] = True
-
-    # define voltages [Volts]
-    Uupper_plate = 0.
-    Ulower_plate = 1e3
 
     # array for electric potential
     U = np.zeros((mx, my, mz))